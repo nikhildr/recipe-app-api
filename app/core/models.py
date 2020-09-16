337591d0bf6887bbbd08ff845279130bf1deb59f
nkumardr <nkumardr@motorola.com>
2020-09-16 17:28:01 +0530

    added flake8

--- app/core/models.py
+++ app/core/models.py
@@ -14,19 +14,20 @@ class UserManager(BaseUserManager):
 
         return user
 
-    def create_superuser(self,email,password=None,**extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         """create and save new superuser"""
-        user= self.model(email=self.normalize_email(email),**extra_fields)
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
-        user.is_staff=True
-        user.is_superuser=True
+        user.is_staff = True
+        user.is_superuser = True
 
         return user
 
+
 class User(AbstractBaseUser, PermissionsMixin):
     """custom user model that support email insted of username"""
-    email = models.EmailField(max_length=255, unique=True,)
+    email = models.EmailField(max_length=255, unique=True, )
     name = models.CharField(max_length=255)
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
